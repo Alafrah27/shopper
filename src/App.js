196109010ccbdf3b7bcddcb62fcf59ad73a9798d
[ali edris] <[almusdarthafa999@gmail.com]>
2024-08-20 14:55:19 -0700

    seven

--- src/App.js
+++ src/App.js
@@ -14,7 +14,9 @@ function App() {
 
   return (
     <BrowserRouter>
-      <div className={` max-w-[1000px]  mx-auto px-3 py-2   `}>
+      <div
+        className={` max-w-[1000px]  mx-auto px-3 py-2  grid grid-rows-[auto_1fr_auto]  `}
+      >
         <Header />
         <div className="  my-4 w-[100%] mx-auto">
           <Routes>
